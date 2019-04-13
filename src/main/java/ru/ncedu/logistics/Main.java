b9d6ab08fae11df7f1e6486baa58c00a3d70aa56
Nikita Gerasimov <dok-strelok@yandex.ru>
2019-04-13 23:49:16 +0400

    Merge branch 'master' of /home/w3/IdeaProjects/2019-logistics with conflicts.

--- src/main/java/ru/ncedu/logistics/Main.java
+++ src/main/java/ru/ncedu/logistics/Main.java
@@ -1,9 +1,10 @@
 package ru.ncedu.logistics;
 
-import ru.ncedu.logistics.model.*;
 import ru.ncedu.logistics.service.DataStorage;
 import ru.ncedu.logistics.service.TestDataInitializer;
 import ru.ncedu.logistics.service.factory.*;
+import ru.ncedu.logistics.service.import_export.ExportedContent;
+import ru.ncedu.logistics.service.import_export.SerializationService;
 import ru.ncedu.logistics.ui.MenuAction;
 
 import java.util.Scanner;
@@ -11,33 +12,6 @@ import java.util.Scanner;
 public class Main {
 
     private final static DataStorage STORAGE = new DataStorage();
-
-    public static void showOfficeInfo(){
-        System.out.println("\nMethod: showOfficeInfo");
-        System.out.println("List of towns:");
-        int number = 0;
-        for(Town element: STORAGE.getAllTowns()){
-            System.out.println(++number + ") " + element.getName());
-        }
-
-        //Selecting town
-        System.out.print("Select town: ");
-        Scanner sc = new Scanner(System.in);
-
-        int selectedTown = sc.nextInt();
-        while (selectedTown < 1 || selectedTown > STORAGE.getAllTowns().size()) {
-            System.out.println("Invalid choice! Make a valid choice: ");
-            selectedTown = sc.nextInt();
-        }
-
-
-        System.out.println("\nList of offices: ");
-        int numberOffice = 0;
-        for(Office element: STORAGE.getAllTowns().get(selectedTown-1).getOffices()){
-            System.out.println("\nOffice #"+ ++numberOffice);
-            element.showOfficeInfo();
-        }
-    }
 
     public static void main(String[] args) {
         System.out.println("\nLogistics 2019. You are welcome!\n");
@@ -55,9 +29,11 @@ public class Main {
             testDataInitializer.createTestData();
         }
 
-        while(true){
+        MenuAction[] actions = MenuAction.values();
+        MenuAction action;
+
+        do{
             //Menu
-            MenuAction[] actions = MenuAction.values();
             System.out.println();
             for(int i = 0; i < actions.length; ++i) {
                 System.out.println(i+1 + ") " + actions[i].getDescription());
@@ -70,7 +46,7 @@ public class Main {
                 act = sc.nextInt();
             }
 
-            MenuAction action = actions[act-1];
+            action = actions[act-1];
             switch (action){
                 case ADD_TOWN:
                     TownFactory townFactory = new TownFactory(STORAGE);
@@ -87,12 +63,15 @@ public class Main {
                 case ADD_ROAD:
                     RoadFactory roadFactory = new RoadFactory(STORAGE);
                     roadFactory.addRoadByUser(); break;
-                case SHOW_INFO: showOfficeInfo(); break;
-                case EXIT:
-                    System.out.println("Programm is halt...");return;
-            }
-        }
-
+                case SHOW_INFO: STORAGE.showOfficeInfo(); break;
+                case FIND_PRODUCT: STORAGE.findProduct(); break;
+                case EXPORT:
+                    SerializationService.serializeDataStorageToFile(new ExportedContent(STORAGE)); break;
+                case IMPORT:
+                    SerializationService.deserializeDataStorageFromFile(STORAGE); break;
+                }
+            } while (action != MenuAction.EXIT);
+            System.out.println("Program is halt...");return;
     }
 
 }
--- src/main/java/ru/ncedu/logistics/service/DataStorage.java
+++ src/main/java/ru/ncedu/logistics/service/DataStorage.java
@@ -1,20 +1,35 @@
 package ru.ncedu.logistics.service;
 
-import ru.ncedu.logistics.model.Office;
-import ru.ncedu.logistics.model.Product;
-import ru.ncedu.logistics.model.Road;
-import ru.ncedu.logistics.model.Town;
+import ru.ncedu.logistics.model.*;
 
+import java.io.Serializable;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
-public class DataStorage {
+public class DataStorage implements Serializable{
 
     private List<Town> towns = new LinkedList<>();
     private List<Product> products = new LinkedList<>();
     private List<Road> roads = new LinkedList<>();
+    private List<Offering> offerings = new LinkedList<>();
 
     public DataStorage(){}
+
+    public void clear(){
+        towns.clear();
+        products.clear();
+        roads.clear();
+        offerings.clear();
+    }
+
+    public void restore(DataStorage dump){
+        this.towns = dump.getAllTowns();
+        this.products = dump.getAllProducts();
+        this.roads = dump.getAllRoads();
+        this.offerings = dump.getAllOfferings();
+    }
 
     public List<Town> getAllTowns() {
         return towns;
@@ -31,13 +46,12 @@ public class DataStorage {
     }
 
     public Product getProductByName(String name){
-        Product temp = new Product("");
         for(Product el: products){
             if(el.getName().equals(name)){
-                temp = el;
+                return el;
             }
         }
-        return temp;
+        return null;
     }
 
     public Office getOfficeByTownAndPhone(String townName, String phone){
@@ -62,6 +76,10 @@ public class DataStorage {
         return products;
     }
 
+    public List<Offering> getAllOfferings(){
+        return offerings;
+    }
+
     public void addTown(Town obj){
         towns.add(obj);
     }
@@ -72,5 +90,68 @@ public class DataStorage {
 
     public void addProduct(Product obj){
         products.add(obj);
+    }
+
+    public void addOffering(Offering obj){
+        offerings.add(obj);
+    }
+
+    public void showOfficeInfo(){
+        System.out.println("\nMethod: showOfficeInfo");
+        System.out.println("List of towns:");
+        int number = 0;
+        for(Town element: getAllTowns()){
+            System.out.println(++number + ") " + element.getName());
+        }
+
+        //Selecting town
+        System.out.print("Select town: ");
+        Scanner sc = new Scanner(System.in);
+
+        int selectedTown = sc.nextInt();
+        while (selectedTown < 1 || selectedTown > getAllTowns().size()) {
+            System.out.println("Invalid choice! Make a valid choice: ");
+            selectedTown = sc.nextInt();
+        }
+
+
+        System.out.println("\nList of offices: ");
+        int numberOffice = 0;
+        for(Office element: getAllTowns().get(selectedTown-1).getOffices()){
+            System.out.println("\nOffice #"+ ++numberOffice);
+            element.showOfficeInfo();
+        }
+    }
+
+    public void findProduct(){
+        System.out.println("\nSearching product by name.");
+        System.out.print("Enter product's name: ");
+        Scanner sc = new Scanner(System.in);
+        Product selectedProduct = getProductByName(sc.nextLine());
+
+        if(selectedProduct != null){
+            List<Offering> findedOfferings = new LinkedList<>();
+
+            for(Offering el: getAllOfferings()){
+                if(el.getProduct().equals(selectedProduct)){
+                    findedOfferings.add(el);
+                }
+            }
+
+            Collections.sort(findedOfferings, new SortByPrice());
+
+            System.out.println("Found offerings:");
+            for (Offering el: findedOfferings){
+                System.out.println("Town: " + el.getOffice().getTown().getName());
+                System.out.println("Phone: " + el.getOffice().getPhone());
+                System.out.println("Price: " + el.getPrice() + "\n");
+            }
+
+        } else {
+            System.out.println("Product doesn't exists!");
+            return;
+        }
+
+
     }
 }
