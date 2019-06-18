6c51aa37ba2488968d1ae174b28407409ede51a0
Roman Semenov <romansemenov3@gmail.com>
2019-06-18 18:06:51 +0400

    Startup fix

---
+++ src/test/java/ru/ncedu/logistics/TestExample.java
@@ -0,0 +1,14 @@
+package ru.ncedu.logistics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestExample {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
